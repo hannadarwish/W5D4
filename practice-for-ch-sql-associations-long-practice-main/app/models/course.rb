7dfb4844b82d6a208d4a108543d0610fa64b9e22
hannadarwish <hannadarwish@gmail.com>
2023-04-27 14:39:31 -0700

    Created associations

---
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+class Course < ApplicationRecord
+    validates :name, :prereq_id, :instructor_id, presence: true
+end
+
---
+++ practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Enrollment < ApplicationRecord
+    validates :course_id, :student_id, presence: true
+
+    belongs_to :student,
+    foreign_key: student_id,
+    primary_key: id,
+    class_name: :User
+    
+end
